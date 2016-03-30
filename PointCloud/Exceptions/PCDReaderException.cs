4de00e5a1b814298ffdbf0b9ff8c30e02b92bfd8
lanybass <lanybass@gmail.com>
2016-03-30 15:35:51 +0800

    fix issue #3 :PCDReaderException.cs missing.

---
+++ PointCloud/Exceptions/PCDReaderException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointCloud.Exceptions
+{
+    public class PCDReaderException : Exception
+    {
+        public PCDReaderException() {}
+
+        public PCDReaderException(String message) : base(message) { }
+    }
+}
